e4cdbccb843c8e72152417b053357fa4cf7b25b8
emilwillems <emilwillems@gmail.com>
2014-12-03 14:08:35 +0100

    moar changes

--- config/config.go
+++ config/config.go
@@ -1,10 +1,46 @@
 package config
 
 import (
+    "os"
+    "log"
+    "io/ioutil"
+    "encoding/json"
 )
 
-function LoadFromFile(filename string, logger *Logger) *Config {
+type Config_IRC struct {
+    Host            string
+    Port            string
+    Nick            string
+    Password        string
+    UseSSL          bool
 
-    logger.Printf("reading configuration from %s\n", filename)
-    
+    AdminChannel    string
+
+    Debug           bool
+}
+
+type Config struct {
+    IRC         Config_IRC    
+}
+
+// Loads configuration from given filename, this function is critical
+// so it will os.exit(1) using log.Fatal when something bad[tm] happens.
+func LoadFromFile(filename string) *Config {
+    var conf Config
+
+    log := log.New(os.Stdout, "[config] ", log.LstdFlags)
+    log.Printf("reading configuration from %s\n", filename)
+
+    file, err := ioutil.ReadFile(filename)
+    if err != nil {
+        log.Fatalf("could not read configuration: %s\n", err)
+    }
+
+    err = json.Unmarshal(file, &conf)
+    if err != nil {
+        log.Fatalf("could not parse configuration: %s\n", err)
+    }
+
+    log.Println("configuration loaded")
+    return &conf 
 }
--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+    "os"
+    "log"
+
+    "./config"
+    "./bot"
+)
 
 func main() {
-        fmt.Printf("Hello, world.\n")
+    logger := log.New(os.Stdout, "[main]   ", log.LstdFlags)
+    config := config.LoadFromFile("bot.conf")
+
+    bot, err := bot.New(config)
+    if err != nil {
+        logger.Printf("FATAL ERROR - unable to init IRC bot: %s\n", err)
+        return
+    }
+
+    err = bot.Connect()
+    if err != nil {
+        logger.Printf("FATAL ERROR - unable to connect to IRC: %s\n", err)
+        return
+    }
+
+    quit := make(chan bool)
+
+    go func() {
+        <- bot.Quitted
+        logger.Println("bot decided to quit")
+        quit <- true
+    }()
+
+    <- quit
+    logger.Println("exiting gracefully")
+    os.Exit(0)
 }

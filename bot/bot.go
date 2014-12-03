ba0bb0506ae927ce442a6b91dc79ceb9986d46d2
emilwillems <emilwillems@gmail.com>
2014-12-03 14:03:21 +0100

    add example config, start working on the actual bot code

---
+++ bot/bot.go
@@ -0,0 +1,69 @@
+package bot
+
+import (
+    "os"
+    "log"
+    "net"
+    "time"
+
+    "../config"
+
+    "github.com/thoj/go-ircevent"
+)
+
+type Bot struct {
+    client      *irc.Connection
+    cfg         *config.Config
+    Log         log.Logger
+    Quitted     chan bool 
+}
+
+func (bot *Bot) Quit() {
+    bot.Quitted <- true
+}
+
+func (bot *Bot) Connect() error {
+    bot.Init()
+    if (bot.cfg.IRC.Port != "") {
+        return bot.client.Connect(net.JoinHostPort(bot.cfg.IRC.Host, bot.cfg.IRC.Port))
+    }
+    return bot.client.Connect(bot.cfg.IRC.Host)
+}
+
+func (bot *Bot) Init() {
+    bot.client.AddCallback("001", bot.JoinChannels)
+}
+
+func New(cfg *config.Config) (*Bot, error) {
+
+    client := irc.IRC(cfg.IRC.Nick, "go-ircbot")
+    client.UseTLS = cfg.IRC.UseSSL
+    client.Log = log.New(os.Stdout, "[irc]    ", log.LstdFlags)
+
+    if cfg.IRC.Password != "" {
+        client.Password = cfg.IRC.Password
+    }
+
+    if (cfg.IRC.Debug) {
+        client.Log.Println("enabling debug mode")
+        client.Debug = true
+    }
+
+    bot := &Bot{
+        cfg:        cfg,
+        client:     client,
+        Quitted:    make(chan bool, 1),
+    }
+
+    return bot, nil
+}
+
+
+// default callbacks
+func (bot *Bot) JoinChannels(event *irc.Event) {
+    time.Sleep(time.Second)    
+
+    // only join adminchannel for now
+    bot.client.Join(bot.cfg.IRC.AdminChannel)
+}
+
